74aa0c560047f9dabd06503e6ee2fa536e355d2b
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-07-12 21:55:50 +0800

    part3a: Middleware

--- practice/part3_backend/index.js
+++ practice/part3_backend/index.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+const requestLogger = (request,response, next)=>{
+  console.log('Method: ',request.method)
+  console.log('Path: ', request.path)
+  console.log('Body: ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+const unknownEndpoint = (request, response) =>{
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+
+app.use(unknownEndpoint)
+
+
 
 let notes = [
   {
