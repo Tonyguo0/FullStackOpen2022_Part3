1e0c9da11d9518be8859fad738b57b702df54cad
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-07-14 23:42:20 +0800

    part3b: heroku fix 6

--- practice/part3_backend/index.js
+++ practice/part3_backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cor = require("cors");
+const cors = require("cors");
 app.use(express.json());
 const requestLogger = (request, response, next) => {
   console.log("Method: ", request.method);
