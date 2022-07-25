06bc7ff51863ab8e64272f93e64dfaf6d6cc4b79
Tony Guo <46705969+Tg0o0@users.noreply.github.com>
2022-07-25 23:32:42 +0800

    part3c: Fetching objects from the database

--- practice/part3_backend/mongo.js
+++ practice/part3_backend/mongo.js
@@ -1,37 +1,56 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url = `mongodb+srv://tgo:${password}@fullstackopen-tony.3qsjiry.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://tgo:${password}@fullstackopen-tony.3qsjiry.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
+// common syntax
+// mongoose.connect(url);
+
+// Note.find({ important: {$in:false}}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+
+//   mongoose.connection.close()
+// });
+
+// promise chaining
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log('connected')
-
-    const note = new Note({
-      content: 'HTML is Easy',
-      date: new Date(),
-      important: true,
-    })
-
-    return note.save()
-  })
   .then(() => {
-    console.log('note saved!')
-    return mongoose.connection.close()
+    console.log("connected");
+
+    // const note = new Note({
+    //   content: 'HTML is Easy',
+    //   date: new Date(),
+    //   important: true,
+    // })
+
+    // return note.save()
+    // console.log("Note.find = \n",Note.find({}));
+    return Note.find({ important: { $in: true } });
   })
-  .catch((err) => console.log(err))
+  .then((result) => {
+    // console.log("\n\n\n\n Result = \n",result);
+    result.forEach((note) => {
+      console.log(note);
+    });
+    return mongoose.connection.close();
+  })
+  .catch((err) => console.log(err));
